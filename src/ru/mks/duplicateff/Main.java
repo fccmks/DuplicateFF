9096ab26d4f77fc6a8419bf972cd0f321525497d
Moiseev Konstantin <moiseevko@mail.ru>
2017-03-30 16:06:57 +0700

    Start.

--- src/ru/mks/duplicateff/Main.java
+++ src/ru/mks/duplicateff/Main.java
@@ -1,7 +1,18 @@
 package ru.mks.duplicateff;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Arrays;
+
 /**
  * Created by moiseev on 30.03.2017.
  */
 public class Main {
+    public static void main(String[] args) {
+        File f = new File("D:\\tmp\\dff");
+        if (f.isDirectory()) System.out.println("It is DIRECTORY!!!");
+        System.out.println(Arrays.asList(f.listFiles().length));
+        
+    }
 }
